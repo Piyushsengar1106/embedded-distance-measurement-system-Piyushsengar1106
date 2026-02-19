152f04e720ef228c46eec836c5f12dd39630395b
piyush0001 <piyushsengar881@gmail.com>
2026-02-19 10:30:01 +0530

    Add mandatory Doxygen documentation for ultrasonic distance measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,38 @@
 #include <Arduino.h>
 
+/**
+ * @file main.cpp
+ * @brief Embedded Distance Measurement using Ultrasonic Sensor
+ * @author Piyush
+ * @date 2026-02-19
+ *
+ * @details
+ * Measures distance using the HC-SR04 ultrasonic sensor and
+ * displays the output on the Serial Monitor.
+ */
+
+// Define TRIG and ECHO pins
 #define TRIG_PIN 9
 #define ECHO_PIN 10
 
+// Variables for measurement
 long duration;
 float distance;
 
+/**
+ * @brief Initializes serial communication and sensor pins.
+ */
 void setup() {
     Serial.begin(9600);
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 
-    Serial.println("Ultrasonic Distance Measurement Started");
+    Serial.println("Ultrasonic Distance Measurement Initialized");
 }
 
+/**
+ * @brief Measures distance and prints it to Serial Monitor.
+ */
 void loop() {
 
     digitalWrite(TRIG_PIN, LOW);
